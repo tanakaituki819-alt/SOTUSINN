e77cb76958c7c9bfdaa0d2fa96f066de074d142a
taidon884 <tadion884@gmail.com>
2026-07-03 12:01:56 +0900

    連打対決シーンクラスを作成

---
+++ 3D_Base/Source/CGameSceneBarrgeEncounter.cpp
@@ -0,0 +1,11 @@
+#include "CGameSceneBarrgeEncounter.h"
+
+CGameSceneBarrgeEncounter::CGameSceneBarrgeEncounter()
+	: m_Barrage_MAX (10)
+
+{
+}
+
+CGameSceneBarrgeEncounter::~CGameSceneBarrgeEncounter()
+{
+}
---
+++ 3D_Base/Source/CGameSceneBarrgeEncounter.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//-----------------------------.
+//	連打対決クラス.
+//-----------------------------.
+class CGameSceneBarrgeEncounter
+{
+public:
+	CGameSceneBarrgeEncounter();
+	~CGameSceneBarrgeEncounter();
+
+private:
+	int m_Barrage_MAX;	//連打MAX値
+};
