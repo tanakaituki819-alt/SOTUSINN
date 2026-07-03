b3b243a2bcf89945b4261fa5efef803dc9634ff6
taidon884 <tadion884@gmail.com>
2026-07-03 12:39:55 +0900

    連打対決クラスの中身いじいじ

--- 3D_Base/Source/CBarrgeEncounter.cpp
+++ 3D_Base/Source/CBarrgeEncounter.cpp
@@ -1,7 +1,10 @@
 #include "CBarrgeEncounter.h"
+#include "CXInput.h"
 
 CBarrgeEncounter::CBarrgeEncounter()
-	: m_Barrage_MAX (10)
+	: m_Barrage_MAX		(10)
+	, m_Barrage1_Count	()
+	, m_Barrage2_Count	()
 
 {
 }
@@ -9,3 +12,29 @@ CBarrgeEncounter::CBarrgeEncounter()
 CBarrgeEncounter::~CBarrgeEncounter()
 {
 }
+
+void CBarrgeEncounter::Init()
+{
+	m_Barrage1_Count = 0;
+	m_Barrage2_Count = 0;
+}
+
+void CBarrgeEncounter::Update()
+{
+	//もしも1のAボタンが押されたら　if()
+	m_Barrage1_Count++;
+	//もしも2のAボタンが押されたら　if()
+	m_Barrage2_Count++;
+	
+	if (m_Barrage1_Count <= m_Barrage_MAX)
+	{
+
+	}
+
+	if (m_Barrage2_Count <= m_Barrage_MAX)
+	{
+
+	}
+
+}
+
--- 3D_Base/Source/CBarrgeEncounter.h
+++ 3D_Base/Source/CBarrgeEncounter.h
@@ -9,6 +9,13 @@ public:
 	CBarrgeEncounter();
 	~CBarrgeEncounter();
 
+	void Init();
+
+	void Update();
+
 private:
 	int m_Barrage_MAX;	//連打MAX値
+
+	int m_Barrage1_Count;	//連打カウント１
+	int m_Barrage2_Count;	//連打カウント２
 };
