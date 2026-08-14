f658c0b70bf1279dd602f3973f2dbfcf471a6ac9
beco <lazy.beco@gmail.com>
2026-08-14 18:14:16 +0900

    ・プレイヤー背景を色付き長方形から巻物に変更、それにあたり位置、大きさの調整 ・プレイヤーアイコンの位置、大きさの調整 ・メダルの位置、大きさの調整

--- 3D_Base/Source/GameScene/GameSceneResult/ResultUI/CResultUI.cpp
+++ 3D_Base/Source/GameScene/GameSceneResult/ResultUI/CResultUI.cpp
@@ -706,10 +706,10 @@ void CResultUI::PlayerBackUI()
     FILE* pFile;
     //stdout（標準出力）を新しく作成したコンソールにリダイレクト
     freopen_s(&pFile, "CONOUT$", "w", stdout);
-    std::cout << ananana2 << std::endl;
+    std::cout << ananana << std::endl;
 
 
-    PlayerBack_Img[0]->SetPosition(D3DXVECTOR3(145, ananana2,0));
+    PlayerBack_Img[0]->SetPosition(D3DXVECTOR3(PLAYER_1_BACK.GetPos()));
     PlayerBack_Img[0]->SetScale(D3DXVECTOR3(PLAYER_1_BACK.GetScl()));
     PlayerBack_Img[0]->SetRotationZ(IMAGE_ROTATION_ANGLE);
     PlayerBack_Img[0]->Render();
