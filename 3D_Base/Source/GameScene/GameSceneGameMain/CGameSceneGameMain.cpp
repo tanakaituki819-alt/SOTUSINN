e528702a57e96ca50cfd717223c583f2e864a395
taidon884 <tadion884@gmail.com>
2026-09-18 09:29:03 +0900

    バグ修正

--- 3D_Base/Source/GameScene/GameSceneGameMain/CGameSceneGameMain.cpp
+++ 3D_Base/Source/GameScene/GameSceneGameMain/CGameSceneGameMain.cpp
@@ -1,6 +1,5 @@
 #include "Game/CGame.h"
 #include "CGameSceneGameMain.h"
-#include "ResultDataManager/CResultDataManager.h"
 
 
 CGameSceneGameMain::CGameSceneGameMain(HWND Hwnd, CDirectX9* Dx9, CDirectX11* Dx11, CCamera* m_Camera)
@@ -124,7 +123,6 @@ void CGameSceneGameMain::Update()
 	//タイマーが0秒になっているかつ鍋に残った具材が0になるまたは、強制終了タイマーが45秒経過したら.
 	if (m_pTimer->GetTimu()<=0 && m_pCIngredientsM->GetIngredientsliveing() <=0 || m_EndTimer >= 60 * 45) 
 	{
-		CResultDataManager::GetInstance()->CaptureFromPlayers(m_pPlayer);
 		SenenChang(enScene::WinnnerResult, CSceneChange::TransitionType::RSRIDE, 60, 60);
 	}
 	//ゲームタイマーが0秒になった.
